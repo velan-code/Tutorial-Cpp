28b6a2f05c6da56c8e4ddfc94b98d4e86336b198
velan-code <senthil0velan@gmail.com>
2026-01-25 11:34:27 +0530

    Datatypes

---
+++ datatype.cpp
@@ -0,0 +1,19 @@
+// Author : velan
+// Date : jan-25-26 at 11:22 am in Indian Standard Time
+// Description : I cover what is dataTypes in cpp
+
+#include <iostream> // Header for I/O
+#include <string.h> // Header for String
+using namespace std;
+
+int main() // _Start
+{
+  int interger = 9;                      // byte : 2
+  float floating = 5.5;                  // byte : 4
+  char character = 'x';                  // byte : 1
+  string sentence = "Welcome all";       // byte : based on length of characters
+  double floating_with_extraSize = 8.50; // byte : 8
+  bool Boolean = true;                   // byte : 1
+
+  return 0; // _Exit
+}
