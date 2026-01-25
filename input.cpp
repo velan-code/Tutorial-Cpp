67ad0cc2a41c30caef484f7771ed795f552a61f2
velan-code <senthil0velan@gmail.com>
2026-01-25 12:11:12 +0530

    Add input.cpp

---
+++ input.cpp
@@ -0,0 +1,15 @@
+// Author : velan
+// Date : Jan-25-26 at 12.05am Indian Standard Time
+// Description : Learning to get input from user for console
+
+#include <iostream> // Header for Console I/O
+using namespace std;
+
+int main() // _Start in assembly
+{
+  int a;    // Allocating a memory to Store user input
+  cin >> a; // "cin" stand for console-input used to get input from console
+
+  cout << a; // Show ouput
+  return 0;  // _End
+}
