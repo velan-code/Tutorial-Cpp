8eab30c908f9dc95a6235c687640aa94fdd89c53
velan-code <senthil0velan@gmail.com>
2026-01-25 10:54:26 +0530

    first program

---
+++ hellow.cpp
@@ -0,0 +1,11 @@
+// Author : velan
+// Date : Jan-1-26
+// Description : Learning C++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hellow world!"; //output << "string"
+  return 0;
+}
